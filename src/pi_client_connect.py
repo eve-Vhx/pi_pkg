fcac0d2b3f09a0c2b728068fdd327c23662e70d6
henry <henry@evevehicles.com>
2023-02-28 11:27:46 -0600

    set for nest11012

--- src/pi_client_connect.py
+++ src/pi_client_connect.py
@@ -5,16 +5,16 @@ from msg_pkg.srv import masterConnect
 class PiConnect:
 
     def __init__(self):
-        rospy.init_node("pi_connect_node")
+        rospy.init_node("pi_connect_node_1")
         print("Ready to connect to the server")
         result = self.connect_to_server()
         print(result)
     
     def connect_to_server(self):
-        rospy.wait_for_service('pi_connect_master')
+        rospy.wait_for_service('nest_pi_connect_master')
         try:
-            pi_connect_client = rospy.ServiceProxy('pi_connect_master', masterConnect)
-            pi_connect_res = pi_connect_client(1)
+            pi_connect_client = rospy.ServiceProxy('nest_pi_connect_master', masterConnect)
+            pi_connect_res = pi_connect_client('NEST11012')
             return pi_connect_res.verified
         except rospy.ServiceException as e:
             print("Connection to server failed")
