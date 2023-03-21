575326d19bc2ef8bd70ffeaa4c08ee09407b822b
henry <henry@evevehicles.com>
2023-03-21 11:46:33 -0500

    added reconnection feature and changed names around

--- src/pi_client_connect_nest.py
+++ src/pi_client_connect_nest.py
@@ -3,10 +3,10 @@ import rospy
 from msg_pkg.srv import masterConnect
 from msg_pkg.msg import uiMasterList
 
-class PiConnect:
+class PiConnectNest:
 
     def __init__(self):
-        rospy.init_node("pi_connect_node")
+        rospy.init_node("pi_connect_nest_node")
         self.id = 'NEST11014'
         self.master_list = []
         rospy.Subscriber('nest_master_list', uiMasterList, self.master_list_cb)
@@ -24,12 +24,14 @@ class PiConnect:
 
             if not exists:
                 self.connect_to_server()
+
+            rospy.sleep(5)
     
     def connect_to_server(self):
         rospy.wait_for_service('nest_pi_connect_master', timeout=rospy.Duration(3))
         try:
             pi_connect_client = rospy.ServiceProxy('nest_pi_connect_master', masterConnect)
-            pi_connect_res = pi_connect_client('NEST11014')
+            pi_connect_res = pi_connect_client(self.id)
             return pi_connect_res.verified
         except rospy.ServiceException as e:
             print("Connection to server failed")
@@ -37,5 +39,5 @@ class PiConnect:
 
 
 if __name__ == '__main__':
-    PiConnect()
+    PiConnectNest()
     rospy.spin()
